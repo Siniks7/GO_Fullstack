a55d19a494b0e69e68ad439c386432ea7e235e56
Nikolay <siniks7@yandex.ru>
2025-06-19 16:50:06 +0700

    Сервис конфигурации (загрузка .env)

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"go_fullstack/config"
 	"go_fullstack/internal/home"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	config.Init()
+	dbConf := config.NewDatabaseConfig()
+	log.Println(dbConf)
 	home.NewHandler(app)
 	app.Listen(":3000")
 }
