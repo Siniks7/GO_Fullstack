2fddb913d3dd1a0c2a5f7fc00a86347d14d02607
Nikolay <siniks7@yandex.ru>
2025-06-23 22:18:54 +0700

    Установка и настройка templ

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,10 @@ import (
 	"go_fullstack/config"
 	"go_fullstack/internal/home"
 	"go_fullstack/pkg/logger"
-	"strings"
 
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
@@ -17,15 +15,7 @@ func main() {
 	config.NewDatabaseConfig()
 	logConfig := config.NewLogConfig()
 	customLogger := logger.NewLogger(logConfig)
-	engine := html.New("./html", ".html")
-	engine.AddFuncMap(map[string]interface{}{
-		"ToUpper": func(c string) string {
-			return strings.ToUpper(c)
-		},
-	})
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	app := fiber.New()
 	app.Use(recover.New())
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: customLogger,
