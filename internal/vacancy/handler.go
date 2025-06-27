3f99b063cf19de482a2117adcdb2242c13994ef5
Nikolay <siniks7@yandex.ru>
2025-06-27 14:45:44 +0700

    Упражнение - Форматирование ошибок

--- internal/vacancy/handler.go
+++ internal/vacancy/handler.go
@@ -2,6 +2,7 @@ package vacancy
 
 import (
 	"go_fullstack/pkg/tadapter.go"
+	"go_fullstack/pkg/validator"
 	"go_fullstack/views/components"
 
 	"github.com/a-h/templ"
@@ -34,7 +35,7 @@ func (h *VacancyHandler) createVacancy(c *fiber.Ctx) error {
 	)
 	var component templ.Component
 	if len(errors.Errors) > 0 {
-		component = components.Notification("Ошибки", components.NotificationFail)
+		component = components.Notification(validator.FormatErrors(errors), components.NotificationFail)
 		return tadapter.Render(c, component)
 	}
 	component = components.Notification("Вакансия успешно создана", components.NotificationSuccess)
---
+++ pkg/validator/errors.go
@@ -0,0 +1,18 @@
+package validator
+
+import (
+	"strings"
+
+	"github.com/gobuffalo/validate"
+)
+
+func FormatErrors(errors *validate.Errors) string {
+	var res string
+	for _, v := range errors.Errors {
+		res = res + strings.Join(v, ", ") + ", "
+		if len(v) > 1 {
+		}
+	}
+	res = strings.TrimRight(res, ", ")
+	return res
+}
